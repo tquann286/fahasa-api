b4f3a416375de95d12128ad819513290925b1bee
tquann286 <quantrung286@gmail.com>
2023-03-25 13:08:10 +0700

    init

---
+++ server.js
@@ -0,0 +1,17 @@
+// JSON Server module
+const jsonServer = require('json-server')
+const server = jsonServer.create()
+const router = jsonServer.router('db/db.json')
+
+// Make sure to use the default middleware
+const middlewares = jsonServer.defaults()
+
+server.use(middlewares)
+server.use(router)
+// Listen to port
+server.listen(3000, () => {
+  console.log('JSON Server is running')
+})
+
+// Export the Server API
+module.exports = server

0c8c1fdf573887b4e81c2cdbd7fc445e5664cead
tquann286 <quantrung286@gmail.com>
2023-04-05 16:18:20 +0700

    add statistic

--- index.js
+++ index.js
@@ -4,6 +4,15 @@ const router = jsonServer.router('db.json')
 const middlewares = jsonServer.defaults()
 const port = process.env.PORT || 3001 // you can use any port number here; i chose to use 3001
 
+router.get('/statistic', (req, res) => {
+  const { books, users } = router.db.value()
+  const total = {
+    books: books.length,
+    users: users.length,
+  }
+  res.jsonp(total)
+})
+
 server.use(middlewares)
 server.use(router)
 
